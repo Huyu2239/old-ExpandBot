fb3f49e9fb3c599cf7d07802aceb709e67b485b0
Huyu2239 <mov.huyu@gmail.com>
2021-05-20 19:33:17 +0900

    add: on_raw_reaction_add

--- cogs/del.py
+++ cogs/del.py
@@ -5,6 +5,10 @@ class Del(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_raw_rection_add(self, payload):
+        return
+
 
 def setup(bot):
     bot.add_cog(Del(bot))
