68bdbd32896c4a97e7ecfb647e0328039d3d0530
Yuto <git@aiotter.com>
2021-06-05 17:37:20 +0900

    変数名を分かりやすくした
    Git.ExpandBot は命名として曖昧だったので修正した

--- cogs/git_pull.py
+++ cogs/git_pull.py
@@ -5,8 +5,7 @@ from discord.ext import commands
 class Git(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        repo = git.Repo()
-        self.ExpandBot = repo.remotes.origin
+        self.repo = git.Repo()
 
     async def cog_check(self, ctx):
         return await self.bot.is_owner(ctx.author)
@@ -14,7 +13,7 @@ class Git(commands.Cog):
     @commands.command()
     async def git_pull(self, ctx):
         msg = await ctx.send('実行中・・・')
-        self.ExpandBot.pull()
+        self.repo.remotes.origin.pull()
         print('pulled')
         await msg.edit(content='完了')
 
