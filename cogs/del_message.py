01205e2c7ecf78a93dd9a3bd534fd86b46f66ea8
Huyu2239 <mov.huyu@gmail.com>
2021-06-23 10:29:04 +0900

    fix: 命名変更

--- cogs/del_message.py
+++ cogs/del_message.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 EMOJI_DELETE_UNQUOTABLE = "\U0001f5d1"
 
 
-class DeleteMessage(commands.Cog):
+class DeleteMessageByReaction(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
