7f7b54b647371130a268dc0877035faf92574b83
冬優 <61175422+Huyu2239@users.noreply.github.com>
2022-01-26 08:56:57 +0000

    add: boolから絵文字に変換する関数

--- libs/utils.py
+++ libs/utils.py
@@ -17,3 +17,11 @@ async def muted_in(message):
         if await MuteConfigs.read(role.id, MutingTarget.ROLE):
             num *= -1
     return num == -1
+
+def convert_to_emoji_from_bool(target_bool):
+    if target_bool is True:
+        return "<:True:850591234283798558>"
+    elif target_bool is False:
+        return "<:False:850591522171912202>"
+    else:
+        return "<:None:850591553688436746>"
